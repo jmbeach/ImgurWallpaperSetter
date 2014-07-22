68241891efe01c222545fd9499dfc2d7f62bf9a5
Jared Beach <jaredbeachdesign@gmail.com>
2014-07-22 12:21:42 -0500

    Initial Commit

---
+++ ImgurWallpaperSetter/WallpaperRetriever.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace ImgurWallpaperSetter
+{
+    public class WallpaperRetriever
+    {
+        public static String imgurTopWallpapersUri = "http://imgur.com/r/wallpapers/top/day";
+        static WebClient client = new WebClient();
+        public static String downloadImgurWallpapersPageAsString()
+        {
+            return client.DownloadString(imgurTopWallpapersUri);
+        }
+        public static HtmlDocument imgurToHtml()
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(downloadImgurWallpapersPageAsString());
+            return doc;
+        }
+        public static Uri mostPopularImgurWallpaper() {
+            var doc = WallpaperRetriever.imgurToHtml();
+            var list = doc.DocumentNode.Descendants("div").Where(d =>
+    d.Attributes.Contains("class") && d.Attributes["class"].Value.Split(' ').Any(b => b.Equals("post"))
+);
+            var first = list.ToArray()[0].FirstChild.NextSibling;
+            String href = (String)first.Attributes["href"].Value;
+            String[] urlParts = href.Split("/"[0]);
+            String imgCode = urlParts[urlParts.Length - 1];
+            String imgUrl = String.Format("http://i.imgur.com/{0}.jpeg", imgCode);
+            Uri imgUri = new Uri(imgUrl);
+            return imgUri;
+        }
+    }
+}
---
+++ UnitTestProject1/WallpaperRetrieverTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ImgurWallpaperSetter;
+using HtmlAgilityPack;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class WallpaperRetrieverTest
+    {
+        [TestMethod]
+        public void testDownloadImgur()
+        {
+            Wallpaper.Set(WallpaperRetriever.mostPopularImgurWallpaper(), Wallpaper.Style.Stretched);
+        }
+    }
+}
